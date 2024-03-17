1b0094177f609aff6c7f757412989370ebac790a
tulparstudyo <teknovipyazilim@gmail.com>
2024-03-17 13:06:37 +0300

    Create MerkezController.php

---
+++ src/App/Http/Controllers/MerkezController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tulparstudyo\Merkez\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class MerkezController extends Controller
+{
+    static function test(){
+        dd("Test");
+    }
+}
